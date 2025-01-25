2886fe13efc27d261877fcfdfb38ee195aa650fd
Roberto Rolo <robertorolo.dev@gmail.com>
2025-01-25 08:10:52 +0200

    Initial commit with changes

---
+++ pages/[...slug].js
@@ -0,0 +1,40 @@
+import { gql } from "@apollo/client"
+import client from "client"
+
+export default function Page(props) {
+    console.log("PAGE PROPS: ",props);
+    
+    return
+    <div>page</div>
+
+}
+
+
+export const getStaticProps = async()=>{
+    return {
+        props:{},
+    }
+}
+
+export const getStaticPaths = async () => {
+    const { data } = await client.query({
+        query: gql`
+        query AllPagesQuery {
+            pages {
+                    nodes 
+                    {
+                        uri
+                    }
+                }
+        }`
+    });
+
+    return {
+        paths: data.pages.nodes.filter(page => page.uri !== "/").map(page =>({
+            params:{
+                slug: page.uri.substring(1, page.uri.length - 1).split("/")
+            },
+        })),
+        fallback:"blocking",
+    };
+};
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,30 @@
-export default function Home() {
-  return <div>Next JS &amp; WordPress course.</div>;
+import { gql } from "@apollo/client";
+import client from "client"
+import { BlockRenderer } from "components/BlockRenderer/BlockRender";
+import { cleanAndTransformBlocks } from "utils/cleanAndTransformBlocks";
+
+export default function Home(props) {
+  console.log("PROPS:", props);
+  
+  return <div> <BlockRenderer blocks={props.blocks}/></div>;
 }
+
+
+export const getStaticProps = async () => {
+  const { data } = await client.query({
+    query: gql`
+    query NewQuery {
+  nodeByUri(uri: "/") {
+    ... on Page {
+      id
+      blocks(postTemplate: false)
+    }
+  }
+}`
+  })
+  return{
+    props: {
+      blocks: cleanAndTransformBlocks(data.nodeByUri.blocks),
+    },
+  };
+};
