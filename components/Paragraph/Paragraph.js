33568857af7b5947b9fccb143de7988e93365330
rolfinho032 <robertorolo.dev@gmail.com>
2025-02-05 18:11:03 +0200

    added paragraph component and url converter

--- components/Paragraph/Paragraph.js
+++ components/Paragraph/Paragraph.js
@@ -1,5 +1,6 @@
 import { getTextAlign } from "utils/fonts"
+import { relativeToAbsoluteUrls } from "utils/relativeToAbsoluteURLs"
 
 export const Paragraph =({textAlign ="left", content, textColor})=>{
-    return <p className={`max-w-5xl mx-auto ${getTextAlign(textAlign)}`} style={{color: textColor}} dangerouslySetInnerHTML={{ __html: content}}/>
+    return <p className={`max-w-5xl mx-auto ${getTextAlign(textAlign)}`} style={{color: textColor}} dangerouslySetInnerHTML={{ __html: relativeToAbsoluteUrls(content) }}/>
 }
